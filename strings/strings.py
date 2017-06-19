a407c29cf9c37d754a8e0245de395e766632a5e2
omaotzu <omarhphillips@gmail.com>
2017-06-19 13:37:09 +0100

    basic numbers

---
+++ strings/strings.py
@@ -0,0 +1,9 @@
+# len function for length
+print(len('Hello'))
+
+string = 'Hello'
+
+# Print second index location
+print(string[1])
+# Print everything from index location to end of string
+print(string[1:])
